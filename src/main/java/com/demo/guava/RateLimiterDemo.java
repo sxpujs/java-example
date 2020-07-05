b2e8d007b9bb6a755dd6b8dae0f855a8b3455247
sxpujs <sxpujs@163.com>
2020-07-05 14:18:21 +0800

    重写限流器的测试方法

--- src/main/java/com/demo/guava/RateLimiterDemo.java
+++ src/main/java/com/demo/guava/RateLimiterDemo.java
@@ -3,37 +3,47 @@ package com.demo.guava;
 import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class RateLimiterDemo {
 
-    final RateLimiter rateLimiter = RateLimiter.create(5.0); // rate is "2 permits per second"
+    static void submitTasks1() {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        RateLimiter rateLimiter = RateLimiter.create(5); // rate is "5 permits per second"
+        IntStream.range(0, 10).forEach(i -> pool.submit(() -> {
+            if (rateLimiter.tryAcquire()) {
+                try {
+                    log.info("start");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            } else {
+                log.warn("限流");
+            }
+        }));
+        pool.shutdown();
+    }
 
-    void submitTasks(List<Runnable> tasks, Executor executor) {
-        for (Runnable task : tasks) {
-            rateLimiter.acquire(); // may wait
-            executor.execute(task);
-        }
+    static void submitTasks2() {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        RateLimiter rateLimiter = RateLimiter.create(5); // rate is "5 permits per second"
+        IntStream.range(0, 10).forEach(i -> pool.submit(() -> {
+            rateLimiter.acquire();
+            log.info("start");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
+        pool.shutdown();
     }
 
     public static void main(String[] args) {
-        RateLimiterDemo demo = new RateLimiterDemo();
-        Executor executor = Executors.newFixedThreadPool(100);
-        List<Runnable> tasks = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            tasks.add(() -> {
-                try {
-                    log.info("start");
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                }
-            });
-        }
-        demo.submitTasks(tasks, executor);
+        //submitTasks1();
+        submitTasks2();
     }
 }
